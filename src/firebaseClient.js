57a4e94925d384886e01f2807952901158eb7458
Bwaaaaaaa <hanip.redmi3@gmail.com>
2024-11-12 19:16:29 +0700

    Update on respond

---
+++ src/firebaseClient.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCh3xb1RRDA7dl9ATSlPmay6bnRMTEFsw",
+  authDomain: "capstone-project-5d14f.firebaseapp.com",
+  projectId: "capstone-project-5d14f",
+  storageBucket: "capstone-project-5d14f.firebasestorage.app",
+  messagingSenderId: "378136426964",
+  appId: "1:378136426964:web:f54417d92fbb62f3dbfe41"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/routes.js
@@ -0,0 +1,70 @@
+const {
+  registerHandler,
+  addTukang,
+  getAlluser,
+  getAllTukang,
+  getDataBeranda,
+  getDetailProfile,
+  searchHandler,
+  loginHandler,
+  addTransaksiHandler,
+  getTransaksiHandler,
+  getRiwayatHandler,
+} = require("./handler");
+
+routes = [
+  {
+    method: "POST",
+    path: "/register",
+    handler: registerHandler,
+  },
+  {
+    method: "GET",
+    path: "/register",
+    handler: getAlluser,
+  },
+  {
+    method: "POST",
+    path: "/tukang",
+    handler: addTukang,
+  },
+  {
+    method: "GET",
+    path: "/tukang",
+    handler: getAllTukang,
+  },
+  {
+    method: "GET",
+    path: "/tukang/{user_id}",
+    handler: getDataBeranda,
+  },
+  {
+    method: "GET",
+    path: "/detailprofile/{user_id}",
+    handler: getDetailProfile,
+  },
+  {
+    method: "GET",
+    path: "/search",
+    handler: searchHandler,
+  },
+  { 
+    method: "POST", 
+    path: "/users/login", 
+    handler: loginHandler,
+  },
+  { method: 'POST', 
+    path: '/addtransaksi', 
+    handler: addTransaksiHandler,
+  },
+  { method: 'GET', 
+    path: '/transaksi/{id_transaksi}', 
+    handler: getTransaksiHandler,
+  },
+  { method: 'GET', 
+    path: '/riwayat/{user_id}', 
+    handler: getRiwayatHandler, 
+  },
+];
+
+module.exports = routes;
