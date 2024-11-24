f52dfecbd7bfe8cecefbe0fed8b55b9aa36da9b7
Bwaaaaaaa <hanip.redmi3@gmail.com>
2024-11-24 12:25:57 +0700

    minor changes on server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
-        port: 8000,
-        host: 'localhost',
+        port: port,
+        host: '0.0.0.0',
     });
 
     server.route(routes);
