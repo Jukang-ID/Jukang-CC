712e869b4e1bd17f8024eb94307d80012e8be4b0
Bwaaaaaaa <hanip.redmi3@gmail.com>
2024-11-24 12:31:09 +0700

    another minor changes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
+    const port = process.env.PORT || 8080;
     const server = Hapi.server({
         port: port,
         host: '0.0.0.0',
